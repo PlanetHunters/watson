06d1d7317b1ced601078d274c818f19c4c6c70eb
martin.dvpj <martin.devora.pajares@gmail.com>
2022-05-07 22:01:52 +0100

    update release

---
+++ release.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+git_tag=$1
+sed -i '6s/.*/version = "'${git_tag}'"/' setup.py
+sed -i '1s/.*/__version__ = "'${git_tag}'"/' watson/__init__.py
+git add setup.py
+git add watson/__init__.py
+git commit -m "Preparing release ${git_tag}"
+git tag ${git_tag} -m "New release"
+git push --tags
