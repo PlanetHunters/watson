2cb45bdd56af833e6c5ab118d39ebfc1d98d4825
martin.dvpj <martin.devora.pajares@gmail.com>
2022-05-08 15:58:15 +0100

    update job name

--- release.sh
+++ release.sh
@@ -5,7 +5,6 @@ sed -i '6s/.*/version = "'${git_tag}'"/' setup.py
 sed -i '1s/.*/__version__ = "'${git_tag}'"/' watson/__init__.py
 git add setup.py
 git add watson/__init__.py
-git commit -m "[ci skip] Preparing release ${git_tag}"
-git push -o ci.skip
+git commit -m "Preparing release ${git_tag}"
 git tag ${git_tag} -m "New release"
-git push --tags
+git push && git push --tags
