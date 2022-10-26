f609a86263ad29677148ad3da75d1cb18fa4f4d9
martin.dvpj <martin.devora.pajares@gmail.com>
2022-10-26 12:04:23 +0100

    update release.sh

--- release.sh
+++ release.sh
@@ -1,11 +1,29 @@
 #!/bin/bash
 
-git_tag=$1
-git pull
-sed -i '6s/.*/version = "'${git_tag}'"/' setup.py
-sed -i '1s/.*/__version__ = "'${git_tag}'"/' watson/__init__.py
-git add setup.py
-git add watson/__init__.py
-git commit -m "Preparing release ${git_tag}"
-git tag ${git_tag} -m "New release"
-git push && git push --tags
+
+rm tests.log
+rm dist* -r
+rm -r .tox
+rm -r .pytest_cache
+rm -r build
+set -e
+tox -r > tests.log
+tests_results=$(cat tests.log | grep "congratulations")
+if ! [[ -z ${tests_results} ]]; then
+  python3.8 -m venv watson-reqs
+  source watson-reqs/bin/activate
+  python3.8 -m pip install pip -U
+  python3.8 -m pip install numpy
+  python3.8 setup.py install
+  python3.8 -m pip list --format=freeze > requirements.txt
+  deactivate
+  git_tag=$1
+  git pull
+  sed -i '6s/.*/version = "'${git_tag}'"/' setup.py
+  sed -i '1s/.*/__version__ = "'${git_tag}'"/' watson/__init__.py
+  git add setup.py
+  git add watson/__init__.py
+  git commit -m "Preparing release ${git_tag}"
+  git tag ${git_tag} -m "New release"
+  git push && git push --tags
+fi
