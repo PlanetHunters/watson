c4306e76aa4e44b18d1f2abd27744b4b4305f7a8
martin.dvpj <martin.devora.pajares@gmail.com>
2022-10-26 14:47:39 +0100

    fix versioning

--- release.sh
+++ release.sh
@@ -13,17 +13,23 @@ if ! [[ -z ${tests_results} ]]; then
   python3.8 -m venv watson-reqs
   source watson-reqs/bin/activate
   python3.8 -m pip install pip -U
-  python3.8 -m pip install numpy
+  python3.8 -m pip install numpy==1.22.3
+  git_tag=$1
+  sed -i '6s/.*/version = "'${git_tag}'"/' setup.py
+  sed -i '1s/.*/__version__ = "'${git_tag}'"/' watson/__init__.py
   python3.8 setup.py install
   python3.8 -m pip list --format=freeze > requirements.txt
   deactivate
-  git_tag=$1
   git pull
-  sed -i '6s/.*/version = "'${git_tag}'"/' setup.py
-  sed -i '1s/.*/__version__ = "'${git_tag}'"/' watson/__init__.py
   git add setup.py
   git add watson/__init__.py
+  git add requirements.txt
   git commit -m "Preparing release ${git_tag}"
   git tag ${git_tag} -m "New release"
   git push && git push --tags
 fi
+rm -R watson-reqs
+rm dist* -r
+rm -r .tox
+rm -r .pytest_cache
+rm -r build
