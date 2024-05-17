4477745fb58057099abede33374afd8d0e51f3e8
martindevora <martin.devora.pajares@gmail.com>
2024-05-17 12:07:14 +0100

    Preparing release 0.10.4

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-version = "0.10.3"
+version = "0.10.4"
 setuptools.setup(
     name="dearwatson", # Replace with your own username
     version=version,
@@ -25,7 +25,7 @@ setuptools.setup(
                         "bokeh==2.4.2", # TPFPlotter dependency
                         'configparser==5.0.1',
                         "extension-helpers==0.1",
-                        "exoml==0.1.11",
+                        "exoml==0.1.13",
                         "imageio==2.9.0",
                         'pyparsing==2.4.7', # Matplotlib dependency
                         "pyyaml==6.0.1",
