f9c2ba4373bdec1837077287ec093aeeb696aafe
martindevora <martin.devora.pajares@gmail.com>
2024-11-01 13:05:15 +0000

    Preparing release 0.12.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-version = "0.12.2"
+version = "0.12.3"
 setuptools.setup(
     name="dearwatson", # Replace with your own username
     version=version,
