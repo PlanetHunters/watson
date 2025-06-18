35f10b4f03f75b354b599a5c45cfc648332d0248
martindevora <martin.devora.pajares@gmail.com>
2025-06-18 13:12:53 +0100

    update dependencies

--- setup.py
+++ setup.py
@@ -22,10 +22,10 @@ setuptools.setup(
     ],
     python_requires='>=3.11',
     install_requires=[
-                        "bokeh==2.4.2", # TPFPlotter dependency
+                        "bokeh==3.7.3", # TPFPlotter dependency
                         'configparser==5.0.1',
                         "extension-helpers==0.1",
-                        "exoml==1.2.0",
+                        "exoml==1.2.1",
                         "imageio==2.9.0",
                         "openai==1.30.1",
                         'pyparsing==2.4.7', # Matplotlib dependency
